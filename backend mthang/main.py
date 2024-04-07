29de4b01f48c3429bc307e7905580fb5522d2f7f
ebear2018 <ejfsohn@Gmail.com>
2024-04-06 20:30:57 -0700

    comments?

--- backend mthang/main.py
+++ backend mthang/main.py
@@ -1,3 +1,4 @@
+#importing librarys and shit
 import os
 from flask import Flask, request
 from flask import jsonify
@@ -5,25 +6,33 @@ from flask import jsonify
 
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-import contents
+from bson.objectid import ObjectId
+
+#for some reason micah says we need this, IMO we should just commit the api keys
 from dotenv import load_dotenv
 
 load_dotenv()
 
 MONGODBKEY = os.getenv('MONGODBKEY')
 print(MONGODBKEY)
+
+#initing the mongodb thing
 dbclient = MongoClient(MONGODBKEY,server_api=ServerApi('1'))
 dbdatabase = dbclient["openkitchen_posts"]
 dbmain = dbdatabase.main
 
+#it working? good, if not, fuck
 try:
     dbclient.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
 
+#i dunno what this does but all the examples have it
 app = Flask(__name__)
 
+#this is bad but im also bad so its all good
+#it takes in an entry id and spits out the corresponding entry
 @app.route('/query', methods=['GET'])
 def getfile():
     filenum = ""
@@ -31,18 +40,22 @@ def getfile():
     if filenum == None:
         filenum = "404 fileid not found"
     retrivedfile = ""
-    retrivedfile = dbmain.find_one({"_id":filenum})
+    retrivedfile = dbmain.find_one({"_id":ObjectId(filenum)})
     if retrivedfile == None:
         retrivedfile = "404 fileid not found"
-    retrivedfile['_id'] = str(retrivedfile['_id'])
+    else:
+        print(retrivedfile)
+        retrivedfile['_id'] = str(retrivedfile['_id'])
     print(retrivedfile)
     outputfile = jsonify(retrivedfile)
     return retrivedfile
 
+#this will make a new file with an author and a title
 @app.route('/query', methods=['POST'])
-def createf():
+def createfile():
     return None
 
+#this allows the site to be open without shitting itself
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
