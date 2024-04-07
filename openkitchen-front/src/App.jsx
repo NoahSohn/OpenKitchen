7add1ba400b8c1b729e16330318397ee484f4427
oxheron <micahogden8@gmail.com>
2024-04-06 20:45:35 -0700

    WOrk

--- openkitchen-front/src/App.jsx
+++ openkitchen-front/src/App.jsx
@@ -1,73 +1,32 @@
 import { useState } from 'react'
 import SearchBar from './search.jsx'
 import Button from "./Button.jsx"
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter, RouterProvider,
+} from "react-router-dom";
 import MyNavbar from './main-navbar.jsx'
 import './App.css'
 import './Button.css'
 import 'bootstrap/dist/css/bootstrap.css';
 
-
 function App() {
   return (
     <>
       <MyNavbar/>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfhjasdkfjkladsjlkfjlasdjfkjslfjlsajdfkljadksfjalkskdfjlkas </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
-      <p> lorem inpisadojiasjdfopijaspdofjiasdnfaslnjdfaljksdhjlfhasdljhfljkdsahfjkladshjklfh </p>
     </>
   );
 }
 
-export default App
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+ {
+    recipe: "/recipe",
+    element: 
+ },
+]);
+
+export default router 
--- openkitchen-front/src/main-navbar.jsx
+++ openkitchen-front/src/main-navbar.jsx
@@ -1,21 +1,31 @@
 import "./navbar.css"
 import React,{useEffect} from 'react';
 import {Navbar, Container, Nav} from 'react-bootstrap';
+import "./image-resize.css"
+import Button from "./Button";
 
-const MyNavbar = () => {
-  return (<Navbar bg="dark" data-bs-theme="dark" sticky="top">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>)
-}
-        // <div className="logo">
-        //   <img src={Logo} alt="Logo" title="Logo" />
-        // </div>
+const MyNavbar = (AccountName, AccountIcon) => {
+    return (
+    <nav className="navbar navbar-dark navbar-expand-lg" style={{backgroundColor: "#333333"}}> 
+        <a className="navbar-brand" href="/home"><img src="logo.png" alt="Logo" width="30" height="24"/></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="/create">
+                <Button isPressed={false} onClick={()=>{}} text="New recipe" defaultStyle="default-button" pressedStyle="pressedButton" defaultImg="new_recipe_unpressed.png" pressedImage="search_pressed.png"/> 
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/search">
+                <Button isPressed={false} onClick={()=>{}} text="Search" defaultStyle="default-button" pressedStyle="pressedButton" defaultImg="search_unpressed.png" pressedImage="search_pressed.png"/> 
+              </a>
+            </li>
+          </ul>
+        </div>
+    </nav>)
+    }
 
 export default MyNavbar 
